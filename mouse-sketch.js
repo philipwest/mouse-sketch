c2e813a655d190e97908e99d1d7ac083a0fe9b17
Philip Westendorp <philip@westendorp.org>
2014-05-31 20:34:14 -0700

    cleaning code, removing grid setup to own file

--- mouse-sketch.js
+++ mouse-sketch.js
@@ -1,45 +1,45 @@
 
 
 $(document).ready(function(){
+	var pixels = 4; //default value for grid
 
-//	var pixels = prompt("How many squares for your Mouse Sketchpad?");
-	var pixels = 4;
+	setup_grid('.sketch-container', pixels); //setup initial grid
+	setup_hover();
 
-	setup_grid($('.sketch-container'), pixels);
-//	setup-hover();
-
+	//assigning prompt for button
 	$('input[name=new-sketchpad]').click(function(){
 		do{
-			pixels = prompt("what size of sketchpad? (4 = 4x4)");
-		}while(!$.isNumeric(pixels) || (pixels <= 0) )
+			//prompt user for new size
+			pixels = prompt("What size of sketchpad? (4 = 4x4)");
+		}while(!$.isNumeric(pixels) || (pixels <= 0))
+			setup_grid('.sketch-container', pixels);	//setup new grid
+			//setup_hover();
+			setup_event('default');
+	});
 
-		$('.sketch-container').empty();
-		setup_grid($('.sketch-container'), pixels);
-	})
+	//assigning prompt for button
+	$('input[name=new-sketchpad-random]').click(function(){
+		do{
+			//prompt user for new size
+			pixels = prompt("What size of sketchpad? (4 = 4x4)");
+		}while(!$.isNumeric(pixels) || (pixels <= 0))
+			setup_grid('.sketch-container', pixels);	//setup new grid
+			//setup_hover();
+			setup_event(2);
+	});
 
+	//assigning prompt for button
+	$('input[name=new-sketchpad-click]').click(function(){
+		do{
+			//prompt user for new size
+			pixels = prompt("What size of sketchpad? (4 = 4x4)");
+		}while(!$.isNumeric(pixels) || (pixels <= 0))
+			setup_grid('.sketch-container', pixels);	//setup new grid
+			//setup_hover();
+			setup_event(1);
+	});
 });
 
-function setup_grid(container, pixel_num){
-	var pixel = '<div></div>';
-	var pixel_dimension = container.width() / pixel_num; 
-
-
-	//Add Pixels to grid
-	for(i = 0, total = Math.pow(pixel_num, 2); i < total; i++){
-		container.append(pixel);	
-	}
-
-	//apply class and dynamic dimensions to pixels
-	container.children().addClass('pixel');
-	$('.pixel').css('width', pixel_dimension).css('height', pixel_dimension);
-
-	//apply class on hover
-	$('.pixel').mouseover(function(){
-		$(this).addClass('activate');
-	//	$(this).css('background-color', getRandomColor());
-
-	})
-}
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF'.split('');
@@ -48,4 +48,34 @@ function getRandomColor() {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
+}
+
+function setup_hover(){
+	//apply class on hover
+	$('.pixel').mouseover(function(){
+		$(this).addClass('activate');
+	//	$(this).css('background-color', getRandomColor());
+
+	});
+}
+
+function setup_event(event_number){
+	switch(event_number){
+		case 1:
+			$('.pixel').click(function(){
+				$(this).toggleClass('activate');
+			});
+			break;
+		case 2:
+			$('.pixel').mouseover(function(){
+				$(this).css('background-color', getRandomColor());
+			});
+			break;
+		default:
+			$('.pixel').mouseover(function(){
+				$(this).addClass('activate');
+			});
+
+	}
+
 }
